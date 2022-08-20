65120a2522b4642daf0019afbf8f4c4016877771
Bin <bin@cacdi.com>
2022-08-20 16:22:31 +0800

    asdf

--- components/Ar/Ar.js
+++ components/Ar/Ar.js
@@ -24,25 +24,37 @@ const Ar = () => {
         <div>Loading, please wait...</div>
       </div>
       {isLoading && (
-        <a-scene
-          vr-mode-ui='enabled: false'
-          embedded
-          renderer='logarithmicDepthBuffer: true'
-          arjs='trackingMethod: best; sourceType: webcam;debugUIEnabled: false'>
-          <a-nft
-            type='nft'
-            url='https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/trex-image/trex'
-            smooth='true'
-            smoothCount='10'
-            smoothTolerance='.01'
-            smoothThreshold='5'>
-            <a-entity
-              gltf-model='https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf'
-              scale='5 5 5'
-              position='50 150 0'></a-entity>
-          </a-nft>
-          <a-entity camera></a-entity>
-        </a-scene>
+        <div>
+          <a-scene>
+            {/* <a-marker preset="hiro"> */}
+            <a-box
+              position='0 1.5 -3'
+              color='#ff0000'
+              material='opacity:0.5'
+              scale='0.5 0.5 0.5'
+              animation='property:rotation; from:0 0 0; to:0 360 0; dur:2000; loop:true; dir: alternate;'></a-box>
+            {/* </a-marker> */}
+            <a-marker preset='hiro'>
+              <a-box
+                color='#0000FF'
+                material='opacity:0.5'
+                // scale="0.5 0.5 0.5"
+                animation={{
+                  property: "rotation",
+                  from: "0 0 0",
+                  to: "0 360 0",
+                  dur: 2000,
+                  loop: true,
+                  dir: "alternate",
+                }}
+                // animation="property:rotation; from:0 0 0; to:0 360 0; dur:2000; loop:true; dir: alternate;"
+              ></a-box>
+            </a-marker>
+            <a-entity camera>
+              <a-cursor></a-cursor>
+            </a-entity>
+          </a-scene>
+        </div>
       )}
       asdSASDASD
     </div>
