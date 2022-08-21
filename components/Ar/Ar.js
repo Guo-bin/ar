3266e47c010bb5ef56358c49d5489a5125b77a30
Bin <bin@cacdi.com>
2022-08-21 16:42:19 +0800

    test43

--- components/Ar/Ar.js
+++ components/Ar/Ar.js
@@ -5,6 +5,8 @@ const Ar = () => {
   const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     setIsLoading(true);
+    document.querySelector("body").style.margin = "0px";
+    document.querySelector("body").style.overflow = "hidden";
   }, []);
   return (
     <div>
@@ -27,8 +29,14 @@ const Ar = () => {
         <div>
           <a-scene
             vr-mode-ui='enabled: false;'
-            renderer='logarithmicDepthBuffer: true;'
+            renderer='logarithmicDepthBuffer: false;'
             arjs='trackingMethod: best; sourceType: webcam;debugUIEnabled: false;'>
+            <a-box
+              position='0 1.5 -3'
+              color='#ff0000'
+              material='opacity:0.5'
+              scale='0.5 0.5 0.5'
+              animation='property:rotation; from:0 0 0; to:0 360 0; dur:2000; loop:true; dir: alternate;'></a-box>
             <a-nft
               type='nft'
               url='https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/trex-image/trex'
@@ -45,6 +53,7 @@ const Ar = () => {
           </a-scene>
         </div>
       )}
+      <div className={"banner"}></div>
       asdSASDASD
     </div>
   );
