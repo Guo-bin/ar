c12633fe122e0191fb1ea764b37ee7aacca651d4
Bin <bin@cacdi.com>
2022-08-21 14:29:11 +0800

    asdfasdf

--- components/Ar/Ar.js
+++ components/Ar/Ar.js
@@ -27,14 +27,14 @@ const Ar = () => {
         <div>
           <a-scene>
             {/* <a-marker preset="hiro"> */}
-            <a-box
+            {/* <a-box
               position='0 1.5 -3'
               color='#ff0000'
               material='opacity:0.5'
               scale='0.5 0.5 0.5'
-              animation='property:rotation; from:0 0 0; to:0 360 0; dur:2000; loop:true; dir: alternate;'></a-box>
+              animation='property:rotation; from:0 0 0; to:0 360 0; dur:2000; loop:true; dir: alternate;'></a-box> */}
             {/* </a-marker> */}
-            <a-marker preset='hiro'>
+            {/* <a-marker preset='hiro'>
               <a-box
                 color='#0000FF'
                 material='opacity:0.5'
@@ -49,7 +49,7 @@ const Ar = () => {
                 }}
                 // animation="property:rotation; from:0 0 0; to:0 360 0; dur:2000; loop:true; dir: alternate;"
               ></a-box>
-            </a-marker>
+            </a-marker> */}
             <a-nft
               type='nft'
               url='https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/trex-image/trex'
